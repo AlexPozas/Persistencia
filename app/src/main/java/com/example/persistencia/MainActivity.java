adef865c67cdafc94b8c3fb8d9418fd8b4942f2b
AlexPozas <97431370+AlexPozas@users.noreply.github.com>
2023-10-30 17:06:40 +0100

    30/10/2023

---
+++ app/src/main/java/com/example/persistencia/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.persistencia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        EditText nom = (EditText) findViewById(R.id.nom);
+        EditText cognom = (EditText) findViewById(R.id.cognom);
+        EditText tel = (EditText) findViewById(R.id.telefon);
+        EditText email = (EditText) findViewById(R.id.email);
+        final Button guardar = findViewById(R.id.button);
+        guardar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String cadena= nom.getText().toString()+","+cognom.getText().toString()+","+tel.getText().toString()+","+email.getText().toString();
+
+
+
+            }
+        });
+
+
+    }
+}
