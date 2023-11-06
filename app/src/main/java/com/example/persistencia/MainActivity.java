319eba8d696b91b9ad68a9d03d2ddefc825a6335
AlexPozas <97431370+AlexPozas@users.noreply.github.com>
2023-11-06 16:04:37 +0100

    30/10/2023

--- app/src/main/java/com/example/persistencia/MainActivity.java
+++ app/src/main/java/com/example/persistencia/MainActivity.java
@@ -8,6 +8,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -19,9 +23,21 @@ public class MainActivity extends AppCompatActivity {
         EditText tel = (EditText) findViewById(R.id.telefon);
         EditText email = (EditText) findViewById(R.id.email);
         final Button guardar = findViewById(R.id.button);
+        Context context = getApplicationContext();
+
         guardar.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+
                 String cadena= nom.getText().toString()+","+cognom.getText().toString()+","+tel.getText().toString()+","+email.getText().toString();
+                try {
+                    FileOutputStream fos = null;
+                    fos = context.openFileOutput("contactes.txt", Context.MODE_PRIVATE);
+                    fos.write(cadena.getBytes(StandardCharsets.UTF_8));
+                }catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+
             }
         });
 
